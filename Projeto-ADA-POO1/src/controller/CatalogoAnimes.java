2c8165bf57e067b77be51e7194fc0c375fdc04e9
fcdmoraes <ffllaa@gmail.com>
2025-08-04 21:33:25 -0300

    Classe CatalogoAnimes feita (1a versão)

---
+++ Projeto-ADA-POO1/src/controller/CatalogoAnimes.java
@@ -0,0 +1,76 @@
+package controller;
+
+import java.util.ArrayList;
+
+public class CatalogoAnimes {
+    private ArrayList<Anime> animes;
+    private ArrayList<Dublador> dubladores;
+    private ArrayList<Dublador> seiyus;
+    private ArrayList<Diretor> diretores;
+    private ArrayList<Estudio> estudios;
+
+    public CatalogoAnimes() {
+        animes = new ArrayList<>();
+        dubladores = new ArrayList<>();
+        seiyus = new ArrayList<>();
+        diretores = new ArrayList<>();
+        estudios = new ArrayList<>();
+    }
+
+    // Métodos de cadastro
+    public void adicionarFilme(Anime anime) {
+        if (!animes.contains(anime)) {
+            animes.add(anime);
+        }
+    }
+
+    public void adicionarDublador(Dublador dublador) {
+        if (!dubladores.contains(dublador)) {
+            dubladores.add(dublador);
+        }
+    }
+
+    public void adicionarSeiyu(Dublador seiyu) {
+        if (!seiyus.contains(dublador)) {
+            seiyus.add(dublador);
+        }
+    }
+
+    public void adicionarDiretor(Diretor diretor) {
+        if (!diretores.contains(diretor)) {
+            diretores.add(diretor);
+        }
+    }
+
+    public void adicionarEstudio(Estudio estudio) {
+        if (!estudios.contains(estudio)) {
+            estudios.add(estudio);
+        }
+    }
+
+    // Associação
+    public void associarDubladorAoFilme(Dublador dublador, Filme filme) {
+        if (filmes.contains(filme) && atores.contains(ator)) {
+            filme.addDublador(ator);
+        }
+    }
+
+    public void associarSeiyuAoFilme(Dublador seiyu, Anime anime) {
+        if (animes.contains(anime) && seiyus.contains(seiyu)) {
+            anime.addSeiyu(seiyu);
+        }
+    }
+
+    public void associarDiretorAoFilme(Diretor diretor, Anime anime) {
+        if (animes.contains(anime) && diretores.contains(diretor)) {
+            anime.setDiretor(diretor);
+        }
+    }
+
+    public void associarEstudioAoFilme(Estudio estudio, Anime anime){
+        if (animes.contains(anime) && estudios.contains(estudio)) {
+            anime.setEstudio(estudio);
+        }
+    }
+}
+
