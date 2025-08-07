2d4eb913f0fb2205282f1ee03d649557886ed33e
fcdmoraes <ffllaa@gmail.com>
2025-08-06 21:50:18 -0300

    Estudio: criação da classe e sobrescrita do método toString

--- Projeto-ADA-POO1/main/src/model/Estudio.java
+++ Projeto-ADA-POO1/main/src/model/Estudio.java
@@ -1,4 +1,22 @@
 package model;
 
+import java.time.format.DateTimeFormatter;
+
 public class Estudio {
+
+    private final String nome;
+
+    public Estudio(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("=== Estudio ===\n");
+        sb.append(String.format("Nome: %s\n", nome));
+
+        return sb.toString();
+    }
 }
